f77e06287ada1dce0398d2ee4c5ebfe1760c799b
DragonOne-3 <jetti4856@gmail.com>
2026-01-19 15:02:29 +0900

    Update munitions_dailydata.py

--- munitions_dailydata.py
+++ munitions_dailydata.py
@@ -20,8 +20,8 @@ def get_yesterday_data():
     
     params = {
         'serviceKey': SERVICE_KEY,
-        'cntrctDateBegin': yesterday,
-        'cntrctDateEnd': yesterday,
+        'cntrctDateBegin': '20240101',
+        'cntrctDateEnd': '20241231',
         'numOfRows': '5000', # 하루치 데이터는 5000건이면 충분함
         'pageNo': '1'
     }
